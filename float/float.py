195adf527559ab0da1001c6834c09b3785022dcc
julian-velez <60607869+julian-velez@users.noreply.github.com>
2025-11-06 09:29:34 -0500

    float_python
    float_python

---
+++ float/float.py
@@ -0,0 +1,6 @@
+# Se crea una variable llamada 'numero' y se le asigna el valor entero 14
+numero = 14
+
+# La función float() convierte el número entero en un número decimal (de tipo float)
+# Es decir, 14 se convierte en 14.0
+print(float(numero))  # Imprime 14.0 en pantalla
